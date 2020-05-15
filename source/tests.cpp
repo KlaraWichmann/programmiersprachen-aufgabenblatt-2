e8dee274bfbd70dde8ad7fa13e5ca09a36bbb55c
Klara Wichmann <klara@imagomat.de>
2020-05-15 11:14:21 +0200

    Task 2.2: vec.cpp + vec.hpp added, defined struct in vec.hpp + tests added for struct in tests.cpp

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+TEST_CASE ("describe_vec2", "[vec2]") {
+
+    Vec2 a; //requires that 0.0f == a.x and 0.0f == a.y
+    REQUIRE (a.x == 0.0f);
+    Vec2 b {5.1f, -9.3f}; /* requires that 5.1f == Approx(b.x) and -9.3f == Approx(b.y)*/
+    REQUIRE (Approx(b.x) == 5.1f);
+    REQUIRE (Approx(b.y) == -9.3f);
+}
+
 
 int main(int argc, char *argv[])
 {
