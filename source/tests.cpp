526cf9634ee9cf0310b6f147ebd50e7fd515bd2a
Klara Wichmann <klara@imagomat.de>
2020-05-15 21:26:37 +0200

    Task 2.3: first operation += & tests & vec.cpp added in CMakeLists.txt

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,12 +3,35 @@
 #include "vec2.hpp"
 
 TEST_CASE ("describe_vec2", "[vec2]") {
-
     Vec2 a; //requires that 0.0f == a.x and 0.0f == a.y
     REQUIRE (a.x == 0.0f);
     Vec2 b {5.1f, -9.3f}; /* requires that 5.1f == Approx(b.x) and -9.3f == Approx(b.y)*/
     REQUIRE (Approx(b.x) == 5.1f);
     REQUIRE (Approx(b.y) == -9.3f);
+}
+
+TEST_CASE ("describe_vec2_operation_add", "[vec2_operation_add]") {
+    //Vector (1, 0) + (0, 0)
+    Vec2 a {1.0f, 0.0f};
+    Vec2 b {0.0f, 0.0f};
+    Vec2 c {1.0f, 0.0f};
+    a += b;
+    REQUIRE(Approx(a.x) == c.x);
+    REQUIRE(Approx(a.y) == c.y);
+    //Vector (5.1, -9.3) + (-5.1, 9.3)
+    a = {5.1f, -9.3f};
+    b = {-5.1f, 9.3f};
+    c = {0.0f, 0.0f};
+    a += b;
+    REQUIRE(Approx(a.x) == c.x);
+    REQUIRE(Approx(a.y) == c.y);
+    //Vector (1.1, 2.2) + (3.3, 4.4)
+     a = {1.1f, 2.2f};
+     b = {3.3f, 4.4f};
+     c = {4.4f, 6.6f};
+     a += b;
+     REQUIRE(Approx(a.x) == c.x);
+     REQUIRE(Approx(a.y) == c.y);
 }
 
 
--- source/vec2.cpp
+++ source/vec2.cpp
@@ -1,1 +1,7 @@
+#include "vec2.hpp"
 
+Vec2& Vec2::operator+= (Vec2 const& v) {
+    x += v.x;
+    y += v.y;
+    return *this;
+}
