bf97dd0d10136e68781b13202aa587f66a8d8f4c
Klara Wichmann <klara@imagomat.de>
2020-05-25 14:40:11 +0200

    Deleted redundancy for mouse position

--- source/example.cpp
+++ source/example.cpp
@@ -11,13 +11,13 @@
 int main(int argc, char* argv[])
 {
     //circle objects saved in an array
-    std::array<Circle, 2> cir = {{
+    std::array<Circle, 2> circles = {{
         {100.0f, Vec2 {200.0f, 200.0f}, Color {1.0f, 1.0f, 1.0f}},
         {100.0f, Vec2 {400.0f, 400.0f}, Color {1.0f, 1.0f, 1.0f}}
     }};
     
     //rectangle objects saved in an array
-    std::array<Rectangle, 3> rec = {{
+    std::array<Rectangle, 3> rectangles = {{
         {Vec2 {0.0f, 0.0f}, Vec2 {100.0f, 100.0f}, Color {1.0f, 1.0f, 1.0f}},
         {Vec2 {100.0f, 50.0f}, Vec2 {200.0f, 300.0f}, Color {1.0f, 1.0f, 1.0f}},
         {Vec2 {100.0f, 50.0f}, Vec2 {300.0f, 300.0f}, Color {1.0f, 1.0f, 1.0f}}
@@ -72,30 +72,27 @@ int main(int argc, char* argv[])
     
     win.draw_text(text_offset_x, text_offset_y, font_size, display_text);
       
-      //see if mouse position is in the rectangle object, if true set reactangle to highlight color
-      float x_rectangle = 0.0f;
-      float y_rectangle = 0.0f;
-      std::tie (x_rectangle, y_rectangle) = win.mouse_position();
-      Vec2 mouse_rec {x_rectangle, y_rectangle};
+      //current mouse position as vector
+      float x_mouse = 0.0f;
+      float y_mouse = 0.0f;
+      std::tie (x_mouse, y_mouse) = win.mouse_position();
+      Vec2 mouse_pos {x_mouse, y_mouse};
       
-      for (int i = 0; i < rec.size(); i++) {
-          if (rec[i].is_inside (mouse_rec)) {
-              rec[i].draw (win, true);
+      //see if mouse position is in the rectangle object, if true set reactangle to highlight color
+      for (int i = 0; i < rectangles.size(); i++) {
+          if (rectangles[i].is_inside (mouse_pos)) {
+              rectangles[i].draw (win, true);
           } else {
-              rec[i].draw (win);
+              rectangles[i].draw (win);
           }
       }
       
       //see if mouse position is in circle object, if true set circle to highlight color
-      float x_circle = 0.0f;
-      float y_circle = 0.0f;
-      std::tie (x_circle, y_circle) = win.mouse_position();
-      Vec2 mouse_circle {x_circle, y_circle};
-      for (int i = 0; i < cir.size(); i++) {
-          if (cir[i].is_inside (mouse_circle)) {
-              cir[i].draw (win, true);
+      for (int i = 0; i < circles.size(); i++) {
+          if (circles[i].is_inside (mouse_pos)) {
+              circles[i].draw (win, true);
           } else {
-              cir[i].draw (win);
+              circles[i].draw (win);
           }
       }
       
